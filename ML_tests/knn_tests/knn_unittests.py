ab454da7718e76c54c97b10d0e44be27ced10fda
Hassan Ashfaq <60746056+Hassan-Ashfaq@users.noreply.github.com>
2023-02-24 18:40:48 +0500

    Update knn_unittests.py

--- ML_tests/knn_tests/knn_unittests.py
+++ ML_tests/knn_tests/knn_unittests.py
@@ -1,8 +1,8 @@
 import sys
+import unittest
 sys.path.append("ML/algorithms/knn")
 from knn import KNearestNeighbor
 
-import unittest
 import numpy as np
 
 class Test_KNN(unittest.TestCase):
@@ -12,20 +12,20 @@ class Test_KNN(unittest.TestCase):
         self.KNearestNeighbor = KNearestNeighbor(2)
         self.KNearestNeighbor.train(self.X , self.y)
         
-    def test_K_value(self):
+    def test_k_value(self):
         self.assertTrue(self.KNearestNeighbor.k==2)
     
-    def test_predict_Loop0(self):
+    def test_predict_loop0(self):
         test = np.array([[4, 3]])
         y_pred = self.KNearestNeighbor.predict(test, num_loops=0)
         self.assertTrue(y_pred==0.0)
     
-    def test_predict_Loop1(self):
+    def test_predict_loop1(self):
         test = np.array([[4, 3]])
         y_pred = self.KNearestNeighbor.predict(test, num_loops=1)
         self.assertTrue(y_pred==0.0)
     
-    def test_predict_Loop2(self):
+    def test_predict_loop2(self):
         test = np.array([[4, 3]])
         y_pred = self.KNearestNeighbor.predict(test, num_loops=2)
         self.assertTrue(y_pred==0.0)
@@ -53,5 +53,4 @@ class Test_KNN(unittest.TestCase):
 
 if __name__ == "__main__":
     print("Running KNN Tests")
-    unittest.main()
     unittest.main()
